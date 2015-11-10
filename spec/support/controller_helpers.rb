5f48658c2dc29231102d2aec674d02641ac904b5
Diego Henrique Domingues <diego.domingues16@gmail.com>
2015-11-10 09:29:13 -0200

    Add some controllers helpers

---
+++ spec/support/controller_helpers.rb
@@ -0,0 +1,25 @@
+module ControllerHelpers
+  extend ActiveSupport::Concern
+
+  def json_response
+    case body = JSON.parse(response.body)
+      when Hash
+        body.with_indifferent_access
+      when Array
+        body
+    end
+  end
+
+  def stub_authentication!
+    request.headers['X-Tango-Api-Token'] = 'fake_token'
+    allow(User).to receive(:find_by).with(hash_including(:token)) { current_user }
+  end
+
+  def current_user
+    @current_user ||= build(:user, email: 'test@tango.com')
+  end
+end
+
+RSpec.configure do |config|
+  config.include ControllerHelpers, type: :controller
+end
