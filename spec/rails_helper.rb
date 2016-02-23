1d0dd5c3fb857fc7bcf54d701e0e77450446ad3c
Diego Henrique Domingues <diego.domingues16@gmail.com>
2016-02-22 23:05:59 -0300

    Adding should matchers settings

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
-require 'shoulda/matchers'
 require 'email_spec'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
